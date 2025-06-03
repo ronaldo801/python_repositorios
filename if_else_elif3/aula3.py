6acc8c86c03ebc7ced6edb33eece41c77e466e30
ronaldo801 <ronaldofoccos@gmail.com>
2025-06-03 19:25:45 -0300

    aula

---
+++ if_else_elif3/aula3.py
@@ -0,0 +1,65 @@
+#Estrutura de Decisão
+#if = se / else = senao
+#elif = senao se
+#maior que >
+#menor que <
+#maior ou igual >=
+#menor ou igual <=
+#igualdade ==
+#diferente !=
+#and = e
+#or = ou
+import os
+os.system("cls")
+
+# idade = 70
+
+# if idade < 16:
+#     print("Não pode votar.")
+# elif 16 <= idade < 18 or idade >= 70:
+#     print("Voto facultativo.")
+# else:
+#     print("Voto obrigatório.")
+
+
+
+# idade = int(input("Digite a idade: "))
+
+# if idade >= 18:
+#     print("Pode Tirar CNH.")
+# else:
+#     print("Inapto. ")
+
+#Verificar se o número é positivo ou negativo
+#Verifiar senha correta
+
+#Peça a idade e classifique:
+#Até 11 > criança
+# de 12 a 17 -> adolescente
+# de 18 a 59 -> adulto
+#60 ou mais -> idoso
+
+# numero = int(input("Digite o número: "))
+# if numero % 2 == 0:
+#     print("Par")
+# else:
+#     print("Ímpar")
+
+
+# senha = 1234
+
+# senha2 = int(input("Digite a senha: "))
+# if senha2 == senha:
+#     print("Correto")
+# else:
+#     print("Incorreto")
+
+# idade = int(input("Digita a idade "))
+# if idade < 11:
+#     print("Criança")
+# elif idade >12 and idade <17:
+#     print("Adolescente: ")
+#     if idade >= 18 and idade > 60:
+#         print("Adulto")
+# else:
+#     print("Idoso")
