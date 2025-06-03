812fabf83a98a638b52bf5af764e521bb973ef2e
ronaldo801 <ronaldofoccos@gmail.com>
2025-06-03 19:09:00 -0300

    automaçao

---
+++ automacao3/automacao1.py
@@ -0,0 +1,28 @@
+import pyautogui
+import time
+
+# pyautogui.press("win")
+# time.sleep(3)
+# pyautogui.write("chorme")
+# time.sleep(3)
+# pyautogui.press("enter")
+
+# pyautogui.hotkey("alt", "f4")
+
+time.sleep(5)
+pyautogui.click(x=1806, y=24)
+time.sleep(3)
+pyautogui.doubleClick(x=41, y=331)
+time.sleep(4)
+pyautogui.write("https://www.to.senac.br/")
+time.sleep(3)
+pyautogui.press("enter")
+time.sleep(3)
+pyautogui.doubleClick(x=969, y=68)
+time.sleep(3)
+pyautogui.doubleClick(x=1191, y=206)
+time.sleep(3)
+pyautogui.doubleClick(x=1218, y=380)
+mudanca
+
+
