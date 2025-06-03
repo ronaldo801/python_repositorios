b345e297f3d2d471abd936ab321a1c1000873518
ronaldo801 <ronaldofoccos@gmail.com>
2025-06-03 19:23:03 -0300

    função texte

---
+++ Funcao3/funcao01.py
@@ -0,0 +1,88 @@
+import os
+os.system("cls")
+
+# def saudação_Ronaldo():
+#     print("Olá, Ronaldo")
+#     print("Tem 10 celulares no estoque")
+
+
+
+# def saudação_Amanda():
+#     print("Olá, Amanda")
+#     print("Tem 08 celulares no estoque")
+
+
+# def saudação_Gisele():
+#     print("Olá, Gisele")
+#     print("Tem 06 celulares no estoque")
+
+# saudação_Ronaldo()
+# saudação_Amanda()
+# saudação_Gisele()
+
+# def somar_numeros():
+#     num1 = 10
+#     num2 = 20
+#     resultado = num1 + num2
+#     print(resultado)
+
+
+
+# def somar_numeros2():
+#     num1 = 5
+#     num2 = 5
+#     resultado = num1 + num2
+#     print(resultado)
+
+# somar_numeros()
+# somar_numeros2()
+
+
+# def somar(a, b):
+#     resultado = a + b
+#     print(resultado)
+# somar(10,20)
+
+# def saudação(nome, quantidade):
+#     print(f"Olá, {nome}")
+#     print(f"Temos {int(quantidade)} celulares no estoque")
+
+# saudação("Mateus", 10)
+# saudação("Amanda", 8)
+# saudação("Gisele", 6)
+
+#Crie uma função chamado dobro que recebe um numero e retorne o dobro desse numero
+
+
+# def receber_numero():
+#     num1 = 2
+#     resultado = num1 * 2
+#     print(resultado)
+# receber_numero()
+
+# def receber_numero(num1):
+
+#     resultado = num1 * 2
+#     print(resultado)
+# receber_numero(2)
+
+# num1 = int(input("Digite um numero: "))
+# def receber_numero():
+#     resultado = num1 * 2
+#     print(resultado)
+# receber_numero()
+
+
+#Função para calcular a media de 04 notas de um aluno
+
+def media():
+    num1 = 7
+    num2 = 8
+    num3 = 9
+    num4 = 6
+    resultado = (num1 + num2 + num3 + num4) / 4
+    print(resultado)
+media()
+
+
+
