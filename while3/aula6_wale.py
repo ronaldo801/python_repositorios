283d8d9b4357d62aec982cf60bee3b90697b8f03
ronaldo801 <ronaldofoccos@gmail.com>
2025-06-03 19:28:36 -0300

    laço_simples

---
+++ while3/aula6_wale.py
@@ -0,0 +1,49 @@
+# x = 1
+# while x <=100:
+#     print(x)
+#     x = x + 1
+
+# Modifique o programa para exibir os numero de 1 a 100
+
+# x = 1
+# while x <=100:
+#     print(x)
+#     x = x + 1
+
+#Modique o programa para exibir os numeros de 50 a 100
+
+# x = 0
+# while x <=100:
+#     print(x)
+#     x = x + 50
+
+#faça um algoritimo para escrever a contagem regressiva de um lançamento de um foquete.
+#O algoritomo deve imprimir 10,9,8...1,0 e Fogo! na tela
+
+import os
+os.system("cls")
+
+
+# numeros pares
+
+# x = 0
+
+# num = int(input("Digite o numero limite: "))
+# while x <= num:
+#     if x % 2 == 0:
+#         print(x)
+#         x += 2
+
+
+# senha_correta = "senac"
+
+# password = input("Insira a senha: ")
+
+# while password != senha_correta:
+#     print("Senha incorreta. Tente novamente.")
+#     password = input("Insira a senha: ")
+
+# print("Senha correta!")
+
+
+
